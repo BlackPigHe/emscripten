f6429f6fac07ec10a5ac83f696282f5381c77b0f
BlackPigHe <hezijiang>
2020-08-01 21:46:48 +0800

    代码仓初始化

---
+++ src/main/java/com/example/emscripten_opus/EmscriptenOpusApplication.java
@@ -0,0 +1,13 @@
+package com.example.emscripten_opus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmscriptenOpusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmscriptenOpusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emscripten_opus/EmscriptenOpusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emscripten_opus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmscriptenOpusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
